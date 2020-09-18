233cb7366255f86486df41236458f790db150d91
Abhishek Srivastava <abhishek08aug@gmail.com>
2020-09-18 09:12:49 +0530

    added integration tests

--- src/test/java/com/football/standings/StandingsApplicationTests.java
+++ src/test/java/com/football/standings/StandingsApplicationTests.java
@@ -1,13 +1,40 @@
 package com.football.standings;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.boot.test.context.SpringBootTest;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
 
+import com.football.standings.pojo.StandingsResponse;
+import com.football.standings.service.StandingsService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
 @SpringBootTest
 class StandingsApplicationTests {
 
+	@Autowired
+	private StandingsService standingsService;
+
 	@Test
-	void contextLoads() {
+	public void testGetStandingsByCountryName() {
+		StandingsResponse standingsResponse = standingsService.getStandingsByCountryName("England");
+		assertThat(standingsResponse.getStandings().size(), is(not(equalTo(0))) );
 	}
 
+	@Test
+	public void testGetStandingsByLeagueName() {
+		StandingsResponse standingsResponse = standingsService.getStandingsByLeagueName("Championship");
+		assertThat(standingsResponse.getStandings().size(), is(not(equalTo(0))) );
+	}
+
+	@Test
+	public void testGetStandingsByTeamName() {
+		StandingsResponse standingsResponse = standingsService.getStandingsByTeamName("Reading");
+		assertThat(standingsResponse.getStandings().size(), is(not(equalTo(0))) );
+	}
 }
